c6c5a45bfc46029658301c515a0893d6d16cc477
Jorge <jorgeluisds1853@gmail.com>
2025-07-25 00:02:18 -0300

    2.0 , Atualização das pastas login,cuourse e banco de dados

---
+++ src/js/pages/course.js
@@ -0,0 +1,7 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(header => {
+  header.addEventListener("click", event => {
+    header.classList.toggle("active");
+  });
+});
